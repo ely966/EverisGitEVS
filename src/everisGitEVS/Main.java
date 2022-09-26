ee45f2320471e791f5c5795486933c72d9030c96
ely966 <46519177+ely966@users.noreply.github.com>
2022-09-26 14:12:09 +0200

    subir proyecto

---
+++ src/everisGitEVS/Main.java
@@ -0,0 +1,12 @@
+package everisGitEVS;
+
+
+public class Main {
+
+	 public static void main( String[] args )
+	    {
+	    	
+	    	System.out.println("Hola mundo!");
+	        
+	    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module everisGitEVS {
+}
